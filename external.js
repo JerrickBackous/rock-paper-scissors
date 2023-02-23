7bbd86aad719e83de38ac9d4d585dec91aae48b4
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-23 13:44:19 -0600

    create html and js files and outline algorithm

---
+++ external.js
@@ -0,0 +1,16 @@
+//create function named getComputerChoice() that randomly returns computer's choice
+function getComputerChoice() {
+
+}
+
+//create function that playes a single round of rock, paper, scissors
+    //playerSelection is the player's choice (case-insensitive) (prompt())
+    //computerSelection is the computer's choice    
+    //return a string that declares the winner, like  "You lose! Paper beats Rock"
+
+//create function named game()
+    //create for loop that runs 5 times
+    //call the playRound function inside loop and displey round winner (console.log(playRound()))
+    //return winner
+
+//use console.log to display winner
