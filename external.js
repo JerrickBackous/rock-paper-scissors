0a0b13b0a72933c3ea0c73b93c8fbcc177714d2c
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-23 14:21:16 -0600

    create playRound function

--- external.js
+++ external.js
@@ -1,14 +1,36 @@
 //create function named getComputerChoice() that randomly returns computer's choice
 function getComputerChoice() {
-    const rps = ["Rock", "Paper", "Scissors"]
-    let guess = Math.floor(Math.random() * rps.length)
-    return rps[guess]
+    const rps = ["rock", "paper", "scissors"];
+    let guess = Math.floor(Math.random() * rps.length);
+    return rps[guess];
 }
 
 //create function that playes a single round of rock, paper, scissors
+function playRound(user, computer) {
     //playerSelection is the player's choice (case-insensitive) (prompt())
+    playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
     //computerSelection is the computer's choice    
+    computerSelection = getComputerChoice();
+    
+    let roundOutcome = "nothing"
     //return a string that declares the winner, like  "You lose! Paper beats Rock"
+    if (playerSelection === computerSelection) {
+        roundOutcome = "Tie";
+        return roundOutcome;
+    } else {
+        if  ((playerSelection == "rock" && computerSelection == "paper") || 
+            (playerSelection == "paper" && computerSelection == "scissors") ||
+            (playerSelection == "scissors" && computerSelection == "rock")) {
+            roundOutcome = "You lose!"
+        } else {
+            roundOutcome = "You win!"
+        }
+    }
+    console.log(roundOutcome)
+    return roundOutcome
+}
+
 
 //create function named game()
     //create for loop that runs 5 times
